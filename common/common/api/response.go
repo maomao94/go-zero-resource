d3ee6922fd96cf1116fe4cb905705d4100794fc3
hehanpeng <287737281@qq.com>
2021-10-13 17:18:33 +0800

    2021-10-13 16:11:02 master

--- common/common/api/response.go
+++ common/common/api/response.go
@@ -4,13 +4,13 @@ const (
 	SUCCESS = 200
 )
 
-type Body struct {
+type Response struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data",omitempty`
 }
 
-func (r *Body) IsSuccess() bool {
+func (r *Response) IsSuccess() bool {
 	if r.Code == SUCCESS {
 		return true
 	} else {
@@ -18,27 +18,27 @@ func (r *Body) IsSuccess() bool {
 	}
 }
 
-func Result(code int, msg string, data interface{}) *Body {
-	return &Body{
+func Result(code int, msg string, data interface{}) *Response {
+	return &Response{
 		Code: code,
 		Msg:  msg,
 		Data: data,
 	}
 }
 
-func Ok() *Body {
+func Ok() *Response {
 	return Result(SUCCESS, "操作成功", map[string]interface{}{})
 }
 
-func OkWithMessage(message string) *Body {
+func OkWithMessage(message string) *Response {
 	return Result(SUCCESS, message, map[string]interface{}{})
 }
 
-func OkWithData(data interface{}) *Body {
+func OkWithData(data interface{}) *Response {
 	return Result(SUCCESS, "操作成功", data)
 }
 
-func OkWithDetailed(data interface{}, message string) *Body {
+func OkWithDetailed(data interface{}, message string) *Response {
 	return Result(SUCCESS, message, data)
 }
 
--- service/oss-endpoint/cmd/api/internal/logic/makebucketlogic.go
+++ service/oss-endpoint/cmd/api/internal/logic/makebucketlogic.go
@@ -2,7 +2,7 @@ package logic
 
 import (
 	"context"
-	"go-zero-resource/common/common/errorx"
+	"go-zero-resource/common/common/api"
 	"go-zero-resource/service/oss-endpoint/cmd/api/internal/svc"
 	"go-zero-resource/service/oss-endpoint/cmd/api/internal/types"
 
@@ -23,10 +23,10 @@ func NewMakeBucketLogic(ctx context.Context, svcCtx *svc.ServiceContext) MakeBuc
 	}
 }
 
-func (l *MakeBucketLogic) MakeBucket(req types.MakeBucketReq) (*types.EmptyReply, error) {
+func (l *MakeBucketLogic) MakeBucket(req types.MakeBucketReq) (*api.Response, error) {
 	// todo: add your logic here and delete this line
-	if true {
-		return nil, errorx.NewDefaultError("error")
-	}
-	return &types.EmptyReply{}, nil
+	//if true {
+	//	return nil, errorx.NewDefaultError("error")
+	//}
+	return api.OkWithData(&types.EmptyReply{}), nil
 }
