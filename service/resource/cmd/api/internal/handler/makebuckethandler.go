cce6599d4b39d8a8930ed72fdb9a736fd7eeba21
hehanpeng <287737281@qq.com>
2021-10-14 14:19:16 +0800

    2021-10-13 16:11:02 master

---
+++ service/resource/cmd/api/internal/handler/makebuckethandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"go-zero-resource/common/api"
+	"net/http"
+
+	"github.com/tal-tech/go-zero/rest/httpx"
+	"go-zero-resource/service/resource/cmd/api/internal/logic"
+	"go-zero-resource/service/resource/cmd/api/internal/svc"
+	"go-zero-resource/service/resource/cmd/api/internal/types"
+)
+
+func makeBucketHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.MakeBucketReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewMakeBucketLogic(r.Context(), ctx)
+		resp, err := l.MakeBucket(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, api.OkWithData(resp))
+		}
+	}
+}
