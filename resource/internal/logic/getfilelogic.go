b8473a8267c2b0f510f055b94d80f642b5399a4f
hehanpeng <hehanpengyy@163.com>
2022-08-29 17:15:26 +0800

    add

--- resource/internal/logic/getfilelogic.go
+++ resource/internal/logic/getfilelogic.go
@@ -2,9 +2,11 @@ package logic
 
 import (
 	"context"
-
+	"gtw/common/ossx"
+	"gtw/model"
 	"gtw/resource/internal/svc"
 	"gtw/resource/pb"
+	"net/http"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +26,33 @@ func NewGetFileLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetFileLo
 }
 
 func (l *GetFileLogic) GetFile(in *pb.GetFileReq) (*pb.GetFileResp, error) {
-	// todo: add your logic here and delete this line
-
-	return &pb.GetFileResp{}, nil
+	ossTemplate, err := ossx.Template(in.TenantId, in.Code, l.svcCtx.Config.Oss.TenantMode, func(tenantId, code string) (oss *model.TOss, err error) {
+		return l.svcCtx.TOssModel.FindOneByTenantIdOssCode(l.ctx, in.TenantId, in.Code)
+	})
+	if err != nil {
+		return nil, err
+	}
+	object, err := ossTemplate.GetObject(in.TenantId, in.BucketName, in.Filename)
+	if err != nil {
+		return nil, err
+	}
+	fileHeader := make([]byte, 512)
+	fileStat, err := object.Stat()
+	if err != nil {
+		return nil, err
+	}
+	object.Read(fileHeader)
+	contentType := http.DetectContentType(fileHeader)
+	filename := fileStat.Key
+	//获取文件名称带后缀
+	//fileNameWithSuffix := path.Base(fileStat.Key)
+	//获取文件的后缀(文件类型)
+	//fileType := path.Ext(fileNameWithSuffix)
+	//获取文件名称(不带后缀)
+	//fileNameOnly := strings.TrimSuffix(fileNameWithSuffix, fileType)
+	return &pb.GetFileResp{
+		Filename:    filename,
+		ContentType: contentType,
+		Stream:      nil,
+	}, nil
 }
