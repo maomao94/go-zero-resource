1d276c3cd1ee70288398c9dc61504193190004a7
hehanpeng <hehanpengyy@163.com>
2022-11-22 13:20:31 +0800

    change

--- model/resource.sql
+++ model/resource.sql
@@ -2,11 +2,13 @@ create table `t_oss`
 (
     `id`          bigint auto_increment,
     `create_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `create_user` bigint       NOT NULL DEFAULT '0' COMMENT '创建人',
     `update_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `update_user` bigint       NOT NULL DEFAULT '0' COMMENT '修改人',
     `del_state`   tinyint(1) NOT NULL DEFAULT '0',
     `version`     bigint       NOT NULL DEFAULT '0' COMMENT '乐观锁版本号',
     `tenant_id`   varchar(12)  NOT NULL default '000000' comment '租户ID',
-    `category`    bigint       NOT NULL DEFAULT '-1' comment '分类',
+    `category`    bigint       NOT NULL DEFAULT '0' comment '分类 1-minio 2-qiniu 3-ali 4-tecent',
     `oss_code`    varchar(32)  NOT NULL DEFAULT '' comment '资源编号',
     `endpoint`    varchar(255) NOT NULL DEFAULT '' comment '资源地址',
     `access_key`  varchar(255) NOT NULL DEFAULT '' comment 'accessKey',
@@ -15,7 +17,7 @@ create table `t_oss`
     `app_id`      varchar(255) NOT NULL DEFAULT '' comment '应用ID',
     `region`      varchar(255) NOT NULL DEFAULT '' comment '地域简称',
     `remark`      varchar(255) NOT NULL DEFAULT '' comment '备注',
-    `status`      bigint       NOT NULL DEFAULT '-1' comment '状态',
+    `status`      bigint       NOT NULL DEFAULT '0' comment '状态 1-开启 2-关闭',
     primary key (`id`),
     UNIQUE KEY `idx_tid_code` (`tenant_id`,`oss_code`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='oss表';
@@ -24,4 +26,4 @@ INSERT INTO resource.t_oss (create_time, update_time, del_state, version, tenant
                             endpoint, access_key, secret_key, bucket_name, app_id, region, remark, status)
 VALUES ('2022-08-25 16:33:42', '2022-08-25 17:20:36', 0, 0, '000000', 1, 'minio-1',
         '127.0.0.1:19000', 'AKIAIOSFODNN7EXAMPLE', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY', 'default', '', '', '',
-        -1);
+        1);
