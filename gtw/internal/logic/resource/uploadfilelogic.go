a3a5b33096e6f293d772fe790e22bb11130e54d4
hehanpeng <hehanpengyy@163.com>
2023-03-10 11:14:09 +0800

    add

--- gtw/internal/logic/resource/uploadfilelogic.go
+++ gtw/internal/logic/resource/uploadfilelogic.go
@@ -42,7 +42,6 @@ func (l *UploadFileLogic) UploadFile(req *types.UploadFileReq) (resp *types.File
 	defer file.Close()
 	logx.Infof("upload file: %+v, file size: %d, MIME header: %+v",
 		fileHeader.Filename, fileHeader.Size, fileHeader.Header)
-	write := bufio.NewReader(file)
 	if err != nil {
 		return nil, err
 	}
@@ -63,20 +62,25 @@ func (l *UploadFileLogic) UploadFile(req *types.UploadFileReq) (resp *types.File
 		return nil, err
 	}
 	defer f.Close()
-	b := make([]byte, 1024)
-	for {
-		n, err := write.Read(b)
-		if err != nil && err != io.EOF {
-			//有一个特殊问题，当一个文件读读完，遇到文件末尾时，它也会返回一个错误，但是此时我已经读到文件末尾EOF，这个错误应该不算错误，所以应该把读到文件末尾这个错误给去掉。
-			return nil, err
-		}
-		if err == io.EOF {
-			break
-		}
-		_, err = f.Write(b[:n])
-		if err != nil {
-			return nil, err
-		}
+	buffer := bufio.NewReader(file)
+	//b := make([]byte, 1024)
+	//for {
+	//	n, err := buffer.Read(b)
+	//	if err != nil && err != io.EOF {
+	//		//有一个特殊问题，当一个文件读读完，遇到文件末尾时，它也会返回一个错误，但是此时我已经读到文件末尾EOF，这个错误应该不算错误，所以应该把读到文件末尾这个错误给去掉。
+	//		return nil, err
+	//	}
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//	_, err = f.Write(b[:n])
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//}
+	_, err = io.Copy(f, buffer)
+	if err != nil {
+		return nil, err
 	}
 	return &types.FileInfo{
 		Name:        fileHeader.Filename,
