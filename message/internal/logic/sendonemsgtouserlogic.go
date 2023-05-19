b449bceb3ffe5460730714bf1c1f0a9c8b5267f9
hehanpeng <hehanpengyy@163.com>
2023-05-19 14:46:53 +0800

    add push gtw

--- message/internal/logic/sendonemsgtouserlogic.go
+++ message/internal/logic/sendonemsgtouserlogic.go
@@ -23,8 +23,8 @@ func NewSendOneMsgToUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 	}
 }
 
-func (l *SendOneMsgToUserLogic) SendOneMsgToUser(in *pb.SendOneMsgToUserReq) (*pb.SendOneMsgToUserRes, error) {
+func (l *SendOneMsgToUserLogic) SendOneMsgToUser(in *pb.SendOneMsgToUserReq) (*pb.SendOneMsgToUserResp, error) {
 	// todo: add your logic here and delete this line
 
-	return &pb.SendOneMsgToUserRes{}, nil
+	return &pb.SendOneMsgToUserResp{}, nil
 }
